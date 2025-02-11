ddcdb92cae0ad18d854f3a8567a13d6693825f83
Dinu98 <alexdinescu888@gmail.com>
2025-02-11 20:19:11 +0200

    Accesing parameters store and secrets manager

--- functions/user/user.js
+++ functions/user/user.js
@@ -13,6 +13,11 @@ module.exports.post = async (event, context) => {
       TEST_VAR: `${process.env.TEST_VAR}`,
       TEST_VAR_FS: `${process.env.TEST_VAR_FS}`,
       TEST_VAR_COMBINED: `${process.env.TEST_VAR_COMBINED}`,
+      DB_USERNAME: `${process.env.DB_USERNAME}`,
+      DB_PASSWORD: `${process.env.DB_PASSWORD}`,
+      DM_ADMIN_SM: `${process.env.DM_ADMIN_SM}`,
+      DB_USERNAME_SM: `${process.env.DM_ADMIN_SM}`,
+      DB_PASSWORD_SM: `${process.env.DM_ADMIN_SM}`,
     })
   }
 
