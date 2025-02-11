09565f93b167ca001a9343e4ff196dd667d0d139
Dinu98 <alexdinescu888@gmail.com>
2025-02-11 23:14:41 +0200

    Get item from table

--- functions/user/user.js
+++ functions/user/user.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const responses = require('../../utils/responses');
-const { createUser } = require('../../utils/dynamoDB');
+const { createUser, getUser } = require('../../utils/dynamoDB');
 const { USERS_TABLE } = process.env;
 
 module.exports.createUser = async (event, context) => {
@@ -11,6 +11,20 @@ module.exports.createUser = async (event, context) => {
 
     const response = await createUser(body, USERS_TABLE);
     console.log('response: ', response);
+
+    return { ...responses.success, body: JSON.stringify(response) };
+  } catch (e) {
+    console.log(e);
+    return { ...responses.error, body: JSON.stringify(e) };
+  }
+};
+
+module.exports.getUser = async (event, context) => {
+  try {
+    console.log(`event: `, event);
+
+    const email = event.pathParameters.email;
+    const response = await getUser(email, USERS_TABLE);
 
     return { ...responses.success, body: JSON.stringify(response) };
   } catch (e) {
--- utils/dynamoDB.js
+++ utils/dynamoDB.js
@@ -3,29 +3,40 @@ const bcrypt = require('bcryptjs');
 
 const dynamoDB = AWS.DynamoDB.DocumentClient();
 
-const createUser = async(item, table) => {
-  
-  try{
-
+const createUser = async (item, table) => {
+  try {
     const time = new Date().toISOString();
     const params = {
-      "TableName": table,
-      "Item": {
+      TableName: table,
+      Item: {
         ...item,
         createdAt: time,
-        updatedAt: time
-      }
-    }
+        updatedAt: time,
+      },
+    };
 
     return dynamoDB.put(params).promise();
-    
-  }catch(e) {
+  } catch (e) {
     console.log(e);
     throw e;
   }
+};
 
-}
+const getUser = async (email, table) => {
+  const params = {
+    TableName: table,
+    Key: {
+      email: email,
+    },
+    ProjectionExpression: 'email, lastName, firstname',
+    ReturnConsumedCapacity: 'TOTAL',
+    ConsistentRead: true,
+  };
+
+  return dynamoDB.get(params).promise();
+};
 
 module.exports = {
-  createUser
-}
+  createUser,
+  getUser,
+};
