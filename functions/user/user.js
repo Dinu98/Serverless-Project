5f6812c913ec24c9ea7d9b5e35314958c35bae01
Dinu98 <alexdinescu888@gmail.com>
2025-02-11 21:18:19 +0200

    Accesing SLS core variable, AWS standard variable, S3 variable, CF output and converting a ssm value to bool

--- functions/user/user.js
+++ functions/user/user.js
@@ -21,8 +21,15 @@ module.exports.post = async (event, context) => {
       DB_PASSWORD_SM: `${process.env.DB_PASSWORD_SM}`,
       DB_USERNAME_FILE: `${process.env.DB_USERNAME_FILE}`,
       DB_PASSWORD_FILE: `${process.env.DB_PASSWORD_FILE}`,
+      //Accesible because we installed dotenv and configured it above
       FROM_ENV: `${process.env.FROM_ENV}`,
+      //Accesible using serverless dotenv plugin
       ENV_USING_PLUGIN: `${process.env.ENV_USING_PLUGIN}`,
+      SLS_VARIABLE: `${process.env.SLS_VARIABLE}`,
+      AWS_VARIABLE: `${process.env.AWS_VARIABLE}`,
+      S3_VARIABLE: `${process.env.S3_VARIABLE}`,
+      CF_SERVICE_ENDPOINT: `${process.env.CF_SERVICE_ENDPOINT}`,
+      STR_TO_BOOL_VARIABLE: `${process.env.STR_TO_BOOL_VARIABLE}`,
     })
   }
 
